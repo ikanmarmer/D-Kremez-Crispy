5f492cb0b93640526f176f53539fd342c2ffc20c
unknown <ikanmarmer@gmail.com>
2025-09-01 17:33:34 +0700

    karyawan panel

---
+++ app/Filament/Karyawan/Resources/Users/Pages/ViewUser.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Karyawan\Resources\Users\Pages;
+
+use App\Filament\Karyawan\Resources\Users\UserResource;
+use Filament\Actions\EditAction;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewUser extends ViewRecord
+{
+    protected static string $resource = UserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            EditAction::make(),
+        ];
+    }
+}
